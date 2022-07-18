850f75341625ade193cf3c462c93703fb3af9a3d
prasanna.rkm <prasanna.rkm@CLAP3026.prodapt.com>
2022-07-18 17:12:48 +0530

    week 1 Assessment

---
+++ prodapt25/src/main/java/com/prodapt/basics/Tester.java
@@ -0,0 +1,12 @@
+package com.prodapt.basics;
+
+
+public class Tester {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    	 
+   SimpleInterest si = new SimpleInterest();
+   si.SimpleInterest(4000,10,1);
+    }
+}
---
+++ prodapt25/src/main/java/com/prodapt/basics/TesterSchoolRecords.java
@@ -0,0 +1,13 @@
+package com.prodapt.basics;
+
+public class TesterSchoolRecords {
+	public static void main(String[] args) {
+        
+    	 SchoolRecords m1= new SchoolRecords();
+    	 m1.dispRecords();
+    	 m1.addRecords(213,"st john school","perungudi");
+    	 m1.addRecords(133,"laurel higher secondary school","Thoraipakkam");
+    	 m1.addRecords(143,"laurel hisher secondary school","guindy");
+
+}
+}
