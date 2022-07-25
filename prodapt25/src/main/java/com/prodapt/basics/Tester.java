2bddbf9b728b64c7c4f3de086c1c3231b40ea1ce
prasanna.rkm <prasanna.rkm@CLAP3026.prodapt.com>
2022-07-25 17:32:20 +0530

    week 2 coding assignment

--- prodapt25/src/main/java/com/prodapt/basics/Tester.java
+++ prodapt25/src/main/java/com/prodapt/basics/Tester.java
@@ -1,12 +1,15 @@
 package com.prodapt.basics;
 
-
 public class Tester {
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-    	 
-   SimpleInterest si = new SimpleInterest();
-   si.SimpleInterest(4000,10,1);
-    }
+	public static void main(String[] args) {
+		Employee e1 = new Employee("prasanna R", 21, "Associate Software Engineer");
+		Employee e2 = new Employee("Janani S", 21, "Associate Software Engineer");
+		Employee e3 = new Employee("Nivetha M", 21, "Associate Software Engineer");
+		 
+		HR_Manager hr = new HR_Manager();
+		hr.process_Records(e1.name, e1.age, e1.designation);
+		hr.process_Records(e2.name, e2.age, e2.designation);
+		hr.process_Records(e3.name, e3.age, e3.designation);
+	}
 }
+	
